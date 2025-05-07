c100c6d98fa54240ac6d2552482b34e3b4fd1c47
olivermjmj <olivermjmj@outlook.dk>
2025-05-08 00:51:01 +0200

    removed unnecesary import of method

--- MaendDerSlaes/src/main/java/com/example/maendderslaes/Controller.java
+++ MaendDerSlaes/src/main/java/com/example/maendderslaes/Controller.java
@@ -5,7 +5,6 @@ import com.example.maendderslaes.util.SoundManager;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.Label;
 
 public class Controller {
 
